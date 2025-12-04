fab67097a6b5e40b0a1eb599f6ef4ed8573b5334
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-12-04 23:12:44 +0000

    Reverse a string solved

--- 125.valid-palindrome.cpp
+++ 125.valid-palindrome.cpp
@@ -7,16 +7,23 @@
 // @lc code=start
 #include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    bool isPalindrome(string s) {
-        if(s.size()==0 || s.size()==1) return true;
-        int l =0;
-        int r = s.size()-1;
-        while(l<r){
-            while(l<r && !isalnum(s[l])) l++;
-            while(l<r && !isalnum(s[r])) r--;
-            if(tolower(s[l]) != tolower(s[r])) return false;
+    bool isPalindrome(string s)
+    {
+        if (s.size() == 0 || s.size() == 1)
+            return true;
+        int l = 0;
+        int r = s.size() - 1;
+        while (l < r)
+        {
+            while (l < r && !isalnum(s[l]))
+                l++;
+            while (l < r && !isalnum(s[r]))
+                r--;
+            if (tolower(s[l]) != tolower(s[r]))
+                return false;
             l++;
             r--;
         }
@@ -24,4 +31,3 @@ public:
     }
 };
 // @lc code=end
-
