598081ec8a44e38d635b555cfb3a70b9da238afa
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-12-16 07:05:57 +0000

    intersection completed

--- 349.intersection-of-two-arrays.cpp
+++ 349.intersection-of-two-arrays.cpp
@@ -10,15 +10,31 @@ using namespace std;
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>ans;
-        for(int i=0;i<nums1.size()-1;i++){
-            for(int j=0;j<nums2.size()-1;j++){
-                if(nums1[i]==nums2[j]){
-                    ans.push_back(nums1[i]);
-                }
+       set<int>s;
+       vector<int>ans;
+       sort(nums1.begin(),nums1.end());
+       sort(nums2.begin(),nums2.end());
+       int first =0;
+       int second =0;
+       while(first<nums1.size() && second<nums2.size()){
+        if(nums1[first]==nums2[second]){
+            s.insert(nums1[first]);
+            first++;
+            second++;
+        }
+        else{
+            if(nums1[first]<nums2[second]){
+                first++;
+            }
+            else{
+                second++;
             }
         }
-        return ans;
+       }
+       for(auto it:s){
+        ans.push_back(it);
+       }
+       return ans;
     }
 };
 // @lc code=end
