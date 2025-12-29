f5051e2de306ece0e8dce0f5109385ef6835195e
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-12-29 22:21:54 +0000

    Remove duplicate from an array and returning index

---
+++ 26.remove-duplicates-from-sorted-array.ts
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=26 lang=typescript
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+// @lc code=start
+function removeDuplicates(nums: number[]): number {
+    let index: number = 1;
+    for(let i=1;i<nums.length;i++){
+        if(nums[i]!==nums[i-1]){
+            nums[index]=nums[i];
+            index++;
+        }
+    }
+    return index;
+    
+};
+// @lc code=end
+
