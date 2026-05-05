6fb674e46509a38f39b0147feb5c79c046ba6b29
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2026-05-05 11:54:12 +0100

    Product of array -(2)

--- productOfArray_exceptS.ts
+++ productOfArray_exceptS.ts
@@ -7,20 +7,13 @@
   of the resulted array with the product of the elements which go after it.
 */
 function productExceptSelf(nums: number[]): number[] {
-  // No division operator
-  // Time complexity: O(n)
-  // Space complexity: O(1)
-    
   let product = 1;
   let result = new Array(nums.length);
     
-  // Modify from start to end
   for (let i = 0; i < nums.length; i += 1) {
     result[i] = product;
     product *= nums[i];
   }
-    
-  // Modify from end to start
   product = 1;
   for (let i = nums.length - 1; i >= 0; i -= 1) {
     result[i] *= product;
